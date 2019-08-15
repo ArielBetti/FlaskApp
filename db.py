45f9359ef66139fd8bd92adb9353c4e81679a7da
ArielBetti <ariel.betti@gmail.com>
2019-08-15 01:09:54 -0300

    Banco de dados local

---
+++ db.py
@@ -0,0 +1,11 @@
+users = {
+    "Ariel": {
+        "name":"Ariel Betti",
+        "lang":"Português BR"
+    },
+    "Scot": {
+        "name":"Scot Dylan",
+        "lang":"Alemão"
+        }
+    }
+
